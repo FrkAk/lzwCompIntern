c209ed7e141cc188a69ce0bad049f51a57800772
FrknAk <f.akbulutlar@gmail.com>
2020-07-16 14:50:37 +0300

    Init

---
+++ src/main/java/lzwComp/Main.java
@@ -0,0 +1,18 @@
+package lzwComp;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+        System.out.println("hello zey");
+        System.out.println("Hello Frkn");
+
+        redisDB.client();
+
+
+
+    }
+
+}
---
+++ src/main/java/lzwComp/redisDB.java
@@ -0,0 +1,37 @@
+package lzwComp;
+
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
+import org.redisson.config.Config;;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.TransportMode;
+
+public class redisDB {
+
+    public static void client(){
+        System.out.println("Connection Initiated");
+        Config config = new Config();
+        config.setTransportMode(TransportMode.EPOLL);
+        config.useClusterServers()
+                // use "rediss://" for SSL connection
+                .addNodeAddress("redis://10.10.10.228:6379");
+
+        RedissonClient redisson = Redisson.create(config);
+        System.out.println("Connection Successful");
+    }
+
+}
+
+
+
+
