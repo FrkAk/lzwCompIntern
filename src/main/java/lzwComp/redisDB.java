148f21c824cf5d264ad3d34df8151f723a6d37e8
FrknAk <f.akbulutlar@gmail.com>
2020-07-16 17:10:52 +0300

    Redis config and succusful connection

--- src/main/java/lzwComp/redisDB.java
+++ src/main/java/lzwComp/redisDB.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
 import org.redisson.api.RMap;
+import org.redisson.api.RSet;
 import org.redisson.config.Config;;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.TransportMode;
@@ -20,15 +21,29 @@ public class redisDB {
 
     public static void client(){
         System.out.println("Connection Initiated");
+
         Config config = new Config();
-        config.setTransportMode(TransportMode.EPOLL);
-        config.useClusterServers()
+        config.setTransportMode(TransportMode.NIO);
+        config.useSingleServer()
                 // use "rediss://" for SSL connection
-                .addNodeAddress("redis://10.10.10.228:6379");
+                .setAddress("redis://10.10.10.228:6379");
 
 
         RedissonClient redisson = Redisson.create(config);
 
+
+
+        RBucket<String> bucket = redisson.getBucket("stringObject");
+        bucket.set("This is the object value");
+
+        RMap<String, String> Map = redisson.getMap("theMap");
+        Map.put("mapKey", "LZW is map value");
+
+        String objValue = bucket.get();
+        System.out.println("The object value is: " + objValue);
+        String mapValue = (String) Map.get("mapKey");
+        System.out.println("The map value is: " + mapValue);
+        redisson.shutdown();
 
 
         System.out.println("Connection Successful");
